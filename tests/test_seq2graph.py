81d767c46f2f42b62dabdf21b4f517a285f59e2d
Richard Pham <phamrichard45@gmail.com>
2025-04-29 09:22:57 -0400

    .

---
+++ tests/test_seq2graph.py
@@ -0,0 +1,16 @@
+from intigers.seq2graph import * 
+import unittest
+
+### lone file test 
+"""
+python3 -m tests.test_seq2graph
+"""
+###
+
+class Seq2GraphMethods(unittest.TestCase):
+
+    def test__methodone(self):
+        assert True 
+
+if __name__ == '__main__':
+    unittest.main()
