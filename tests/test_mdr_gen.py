fc56357b1c88ba07279aab537b1c6d052b06d82b
Richard Pham <phamrichard45@gmail.com>
2025-05-09 05:37:49 -0400

    remove unnecessary comments from <tests/test_mdr_gen>

--- tests/test_mdr_gen.py
+++ tests/test_mdr_gen.py
@@ -46,7 +46,6 @@ class MDRGenMethods(unittest.TestCase):
 
         mdrg = MDRGen(mdr,prg,False,gentype=2)
 
-        # case 1 
         intseed_cycle = [5]
         mdrg.generate_sequence__type_novelgen(intseed_cycle,\
             max_sequences=100,clear_prev_info=True)
@@ -59,8 +58,7 @@ class MDRGenMethods(unittest.TestCase):
             assert q[i] == q2 
 
     def test__MDRGen__next_gentype2__case2(self):
-
-        # case 2 
+        
         xt = [2,27,672,3232,3232,2,44,215,215,215,2,5,11,23,71,647,2741,13705,5,49]
         #random.seed(100)
         prg = prgen(2,100)
@@ -72,7 +70,6 @@ class MDRGenMethods(unittest.TestCase):
         mdrg = MDRGen(mdr,prg,False,gentype=2,gt2_rcswitch=True,\
             gt2_sel1=True,gt2_sel2=True,gt2_sel3=False)
 
-        # case 1 
         intseed_cycle = [5]
         mdrg.generate_sequence__type_novelgen(intseed_cycle,\
             max_sequences=100,clear_prev_info=True)
