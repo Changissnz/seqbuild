2bbb3058708a8cab6904603e34c749a5625bd1d1
Richard Pham <phamrichard45@gmail.com>
2025-05-17 12:43:10 -0400

    add first test for file<poly_output_fitter_.py>

---
+++ tests/test_poly_output_fitter_.py
@@ -0,0 +1,41 @@
+from intigers.poly_output_fitter_ import * 
+import unittest
+
+### lone file test 
+"""
+python3 -m tests.test_poly_output_fitter_
+"""
+###
+class PolyOutputFitterMethods(unittest.TestCase):
+
+    def test__PolyOutputFitterVar2__solve(self):
+        n = 5 
+        x1,x2 = -3,7
+        pofv = PolyOutputFitterVar2(n,x1,x2,coeff=1,prng=None)
+        pofv.solve() 
+        assert pofv.apply(x1) == pofv.apply(x2) 
+
+        n = 5
+        x1,x2 = 3,7
+        pofv = PolyOutputFitterVar2(n,x1,x2,coeff=1,prng=None)
+        pofv.solve() 
+        assert pofv.apply(x1) == pofv.apply(x2) 
+
+        n = 4
+        x1,x2 = -6,9
+        pofv = PolyOutputFitterVar2(n,x1,x2,coeff=1,prng=None)
+        pofv.solve() 
+        assert pofv.apply(x1) == pofv.apply(x2) 
+
+        n = 3
+        x1,x2 = 2,5
+        pofv = PolyOutputFitterVar2(n,x1,x2,coeff=1,prng=None)
+        pofv.solve() 
+        assert pofv.apply(x1) == pofv.apply(x2) 
+
+
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
