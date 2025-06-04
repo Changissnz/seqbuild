26f8d1385ddedff3a231435e7c52d0993aa4d13a
Richard Pham <phamrichard45@gmail.com>
2025-06-04 16:04:04 -0400

    minor additions to <idt_proc>

--- intigers/idt_proc.py
+++ intigers/idt_proc.py
@@ -8,6 +8,9 @@ class IDTProc:
         self.tn = tn
         self.proc_mode = proc_mode
 
+    """
+    produce a path that v undergoes starting at `tn`. 
+    """
     def process_value(self,v):
         tn = self.tn 
         path = [tn.idn] 
@@ -19,7 +22,50 @@ class IDTProc:
             tn2 = tn.fetch_conn(q)
             path.append(tn2.idn)
             tn = tn2 
-        return path 
+        return path
 
-    def isomorphism(self,S1,S2):
-        return -1 
+    """
+    sequence of output values from the `entryf` functions applied 
+    on `v` if `v` were to travel the sequence of nodes `vpath`. 
+
+    """
+    def travel_path(self,v,vpath):
+
+        xr = self.tn 
+
+        def add_entry(v_): 
+            # print out first value 
+            vx = xr.entryf(v_)
+            if type(vx) == np.ndarray:
+                vx = vx.flatten()
+            else: 
+                vx = [vx]
+            V.extend(vx)
+
+        V = []   
+        add_entry(v) 
+
+        for v2 in vpath: 
+            xr = xr.fetch_conn(v2)
+            add_entry(v2)
+        return V
+
+    """
+    travels `v2` on the path `p` that `v1` 
+    takes from `process_value`. Outputs the
+    corresponding `entryf` values. 
+    """
+    def iso_output(self,v1,v2):
+        p = self.process_value(v1)
+        return self.travel_path(v2,p[1:])
+
+    """
+    outputs the map `node idn` -> `number of input elements that traveled it. 
+    """
+    def traffic_map(self,S):
+        D = defaultdict(int)
+        for s in S:
+            pth = self.process_value(v)
+            pth = dict({(p,1) for p in pth})
+            D = numberdict_op(D,pth,add)
+        return D  
