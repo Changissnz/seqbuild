77c8811ca51eeb680e85c0dfcf9f17d9aaeaf041
Richard Pham <phamrichard45@gmail.com>
2025-06-06 22:04:13 -0400

    made new folder<future> for algorithms planned for some future

---
+++ future/poce_gen.py
@@ -0,0 +1,10 @@
+"""
+Future generator. 
+
+POCE is shorthand for `Public Or Class Enemy`.
+"""
+
+class PublikOKlausEnemy:
+
+    def __init__(self): 
+        return -1 
---
+++ future/rs_gen.py
@@ -0,0 +1,10 @@
+"""
+Future generator. 
+
+RS is shorthand for `Reddest Spectre`.
+"""
+
+class ReddestSpectre:
+
+    def __init__(self): 
+        return -1 
