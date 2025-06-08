16eb4473413395fc730f8779467f7cc964f8ead4
Richard Pham <phamrichard45@gmail.com>
2025-06-08 16:08:48 -0400

    .

--- future/demokrazi_gen.py
+++ future/demokrazi_gen.py
@@ -8,3 +8,11 @@ class DieDemoKrazies:
 
     def __init__(self): 
         return -1 
+
+    def democratized_and_broke(self):
+        print("program it yourself")
+        return -1 
+
+    def wants_promises_fulfilled_gets_ads_instead(self):
+        print("go watch Mad Men")
+        return -1 
