8455be6454193b8fdb440d3e63dbd7d23c644552
Richard Pham <phamrichard45@gmail.com>
2025-06-09 14:49:25 -0400

    .

--- future/rs_gen.py
+++ future/rs_gen.py
@@ -8,7 +8,7 @@ class ReddestSpectre:
 
     def __init__(self): 
         print("RED SAMURAI MUST DIE.")
-        return -1 
+        return  
 
     def die_red_samurai(self): 
         print("HELLO RED SPECTRE.")
---
+++ future/sk_gen.py
@@ -0,0 +1,15 @@
+"""
+Future generator. 
+
+SK stands for `Silent Killer`
+"""
+
+class SKGen:
+
+    def __init__(self):
+        print("silent night...silent night...")
+        self.silent_kill()
+    
+    def silent_kill(self): 
+        print("silent k------awghwhghgwhghw.")
+        print("*silence*")
