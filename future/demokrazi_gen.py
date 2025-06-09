e5e3025a1c956e2f2a00806de0e576bdf317044a
Richard Pham <phamrichard45@gmail.com>
2025-06-09 14:21:48 -0400

    .

--- future/demokrazi_gen.py
+++ future/demokrazi_gen.py
@@ -8,7 +8,7 @@ class DieDemoKrazies:
 
     def __init__(self): 
         print("remember the pandemic,sucker.")
-        return -1 
+        return 
 
     def democratized_and_broke(self):
         print("program it yourself")
@@ -17,3 +17,6 @@ class DieDemoKrazies:
     def wants_promises_fulfilled_gets_ads_instead(self):
         print("go watch Mad Men")
         return -1 
+
+    def till_the_next_day_hero(self): 
+        print("till the next day...hero!!!")
