119aa1295aa35f912c2f254d659f11b12e9c9791
Richard Pham <phamrichard45@gmail.com>
2025-06-09 14:27:18 -0400

    .

--- future/finkrat_gen.py
+++ future/finkrat_gen.py
@@ -7,3 +7,6 @@ class FinkRatGen:
     def __init__(self):
         print("can die somewhat good or as a fink rat.")
         print("fink rat")
+    
+    def sorry_excuse_for_justice(self): 
+        print("ECHO: what a sorry excuse for justice.")
