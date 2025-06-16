fdc3586d7c1565d7d252e01b92d81b6bedd8dacd
Richard Pham <phamrichard45@gmail.com>
2025-06-15 20:30:44 -0400

    add more methods to file<lcg_v3>,file<tvec>

--- intigers/lcg_v3.py
+++ intigers/lcg_v3.py
@@ -3,4 +3,34 @@ from .tvec import *
 
 class LCGV3(LCGV2): 
 
-    def __init__(self): 
+    def __init__(self,start,m,a,n0,n1,sc_size:int,preproc_gd:bool=False):
+        super().__init__(start,m,a,n0,n1,sc_size,preproc_gd)
+        self.tv = None
+
+    def set_tv(self,tv):
+        assert type(tv) == TrinaryVec
+        self.tv = tv 
+        return
+
+    def autoset_tv(self,gen_type,l,ext_prg=None):
+        assert gen_type in {1,2}
+
+        qx = ext_prg if type(ext_prg) != \
+            None else self.__next__ 
+
+        tv = None
+        if gen_type == 1:
+            L = [-1,0,1]
+            bv = L[qx() % 3]
+            cr = (qx() % 1000) / 1000.0 
+            tv = TrinaryVec.one_instance__v1(bv,l,cr,qx) 
+        else: 
+            k0 = (qx() % 1000) / 1000.0
+            k1 = (qx() % 1000) / 1000.0
+            k2 = (qx() % 1000) / 1000.0
+            s = k0 + k1 + k2 
+            k0,k1,k2 = zero_div0(k0,s),\
+                zero_div0(k1,s),zero_div0(k2,s)
+            fm = {-1:k0,0:k1,1:k2}
+            tv = TrinaryVec.one_instance__v2(l,fm,qx)
+        self.tv = tv 
--- intigers/tvec.py
+++ intigers/tvec.py
@@ -1,10 +1,55 @@
+from .seq_struct import * 
+from morebs2.numerical_generator import prg_choose_n
 
-
-class TrinaryVec:
+class TrinaryVec(IntSeq):
 
     def __init__(self,l):
         assert set(l).issubset({0,1,-1})
-        self.l = l  
+        super().__init__(l)
+        
+    # TODO: test these two methods
+    @staticmethod
+    def one_instance__v1(base_value,length,change_ratio,prg): 
+        assert base_value in {-1,0,1}
+        assert change_ratio >= 0.0 and change_ratio <= 1.0 
+
+        vec = np.zeros((length,),dtype=np.int32) \
+            + base_value 
+
+        num_changes = int(ceil(change_ratio * length))
+        Q = [i for i in range(length)]
+        ix = prg_choose_n(Q,n,prg,is_unique_picker=True)
+
+        L = {-1,0,1} - set([base_value]) 
+        L = sorted(L) 
+
+        for i in ix:
+            d = L[prg() % 2]
+            vec[i] = d 
+        return TrinaryVec(vec)
+
+    @staticmethod
+    def one_instance__v2(length,frequency_map,prg): 
+        assert 1.0 - sum(frequency_map.values()) < 10 ** -5 
+        assert set(frequency_map.keys()).issubset({-1,0,1})
+        rx = [(k,int(round(v * length))) for k,v in frequency_map.items()]
+        rx = np.array(rx,dtype=np.int32) 
+        sx = length - sum(rx[:,1])
+
+        # choose random index to add calibration to 
+        if sx != 0:
+            rp = prg() % len(rx) 
+            rx[rp,1] = rx[rp,1] + sx 
+
+        Q = [i for i in range(length)]
+        vec = np.zeros((length,),dtype=np.int32)
+
+        for q in rx:
+            k,v = q[0],q[1] 
+            q2 = prg_choose_n(Q,v,prg,is_unique_picker=True)
+            vec[q2] += k
+
+        return TrinaryVec(vec)
 
     """
     calculates rightward contiguous range for l[i]. 
