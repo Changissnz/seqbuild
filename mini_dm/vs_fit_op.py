2ddcc5f79b4c3582e60ffa95f6d75ee487e4946c
Richard Pham <phamrichard45@gmail.com>
2025-06-28 19:23:10 -0400

    start new class<MADHyp>

--- mini_dm/vs_fit_op.py
+++ mini_dm/vs_fit_op.py
@@ -1,5 +1,11 @@
 from .vs_fit import * 
 
+class MADHyp(MADescriptor):
+
+    def __init__(self,rv_vec,rvt_vec,t_vec,s_vec,d_vec,ma_order=None):
+        super().__init__(rv_vec,rvt_vec,t_vec,s_vec,d_vec,ma_order)
+        return 
+
 class VSTransform:
 
     def __init__(self):
