6ad02d385b91179a97a3a8c1463095448c3725fa
Richard Pham <phamrichard45@gmail.com>
2025-07-27 12:31:40 -0400

    add new class<AbstractSeq>

---
+++ mini_dm/absseq.py
@@ -0,0 +1,10 @@
+"""
+uses concepts derived from Kolmogorov complexity to measure certain 
+qualities of sequences in various representative forms. 
+"""
+
+class AbstractSeq:
+
+    def __init__(self):
+
+        return 
