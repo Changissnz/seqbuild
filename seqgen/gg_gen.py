3982053a1f6729ada42f7284fc026e5701aaa55a
Richard Pham <phamrichard45@gmail.com>
2025-08-02 11:57:12 -0400

    about to add density feature to class<AGV2GuidedGen>

--- seqgen/gg_gen.py
+++ seqgen/gg_gen.py
@@ -1,5 +1,18 @@
 from desi.multi_metric import * 
 
+def mod_uwpd_of_sequence(S,m):
+    assert m != 0 
+    S_ = np.array([s_ % m for s_ in S]) 
+    return uwpd(S_,pairwise_op=lambda x1,x2: np.abs(x2 - x1),\
+        accum_op=lambda x1,x2: x1 + x2)
+
+def factorseq_to_uwpdcov_map(seq,fseq):
+    uwpdcov = dict()
+    for m in fseq: 
+        pdseq = mod_uwpd_of_sequence(seq,m)
+        covseq = coverage_of_sequence(seq,[0,m],max_radius=0.5)
+        uwpdcov[m] = (pdseq,covseq)
+    return uwpdcov
 
 """
 container to hold 
@@ -15,27 +28,22 @@ class AGV2DensityLog:
 
         self.covuwpd_log = [] 
         self.moduwpd_log = [] 
+
+        self.factormap_log = defaultdict(list) 
         self.refvar = None
-        return -1 
+        return 
 
-def mod_uwpd_of_sequence(S,m):
-    assert m != 0 
-    S_ = np.array([s_ % m for s_ in S]) 
-    return uwpd(S_,pairwise_op=lambda x1,x2: np.abs(x2 - x1),\
-        accum_op=lambda x1,x2: x1 + x2)
+    def update_covuwpd(self,cov,uwpd): 
+        self.covuwpd_log.append((cov,uwpd)) 
 
-def factorseq_to_uwpdcov_map(seq,fseq):
-    uwpdcov = dict()
-    for m in fseq: 
-        pdseq = mod_uwpd_of_sequence(seq,m)
-        covseq = coverage_of_sequence(seq,[0,m],max_radius=0.5)
-        uwpdcov[m] = (pdseq,covseq)
-    return uwpdcov
-
+    def update_factormap(self,d):
+        for k,v in self.d.items():
+            self.factormap_log[k].append(v)
+    
 class AGV2GuidedGen: 
 
     def __init__(self,base_prg,aux_prg,\
-        base_output_span,density_measure):
+        base_output_span,density_measure,ngram_length=None,):
 
         self.base_prg = base_prg 
         self.aux_prg = aux_prg 
@@ -50,7 +58,10 @@ class AGV2GuidedGen:
         self.set_span(base_output_span)
         self.set_density(density_measure) 
 
+        self.ngram_length = ngram_length
         self.output_mode = "base" 
+
+        self.init_density_log() 
         return 
 
     def set_span(self,span):
@@ -93,21 +104,28 @@ class AGV2GuidedGen:
         assert len(self.base_seq) > 0
         return -1 
 
-    def inspect_base_seq(self,ngram=None):
-        if type(ngram) == type(None): 
-            ngram = default_ngram_length(len(self.base_seq))  
-        mm = MultiMetric(self.base_seq)
-        smry = mm.summarize(ngram,False)
+    def inspect_base_seq(self):
+        if type(self.ngram_length) == type(None): 
+            self.ngram_length = default_ngram_length(len(self.base_seq))  
+        
+        q = self.seq_summary(self.base_seq,True)  
+        self.bs_summary = q 
+        return self.bs_summary 
+
+    def init_density_log(self):
+        self.agd_log = AGV2DensityLog() 
+        return
+
+    def seq_summary(self,seq,full_output:bool=False):
+        mm = MultiMetric(seq)
+        smry = mm.summarize(self.ngram_length,False)
+
+        if not full_output:
+            return [smry,None] 
 
         mm.load_mc_map()
         mcm = mm.modcomplex_map 
-        self.bs_summary = [smry,mcm]
-        return self.bs_summary 
-
-    def load_into_log(self):
-        # interested only in [0][0&1] 
-        cov = self.bs_summary[0][0] 
-        uwpd_ = self.bs_summary[0][1]
+        return [smry,mcm]
 
     def reload_mcm(self):
         return -1 
@@ -115,4 +133,13 @@ class AGV2GuidedGen:
     def factor_kcomplexity(self,seq):
         qmcm = sorted(list(self.bs_summary[1].keys()))
         return factorseq_to_uwpdcov_map(self.base_seq,qmcm)
-        
+    
+    def log_seq(self,seq): 
+        summry = self.seq_summary(seq,False)[0] 
+        fmap = self.factor_kcomplexity(seq)
+
+        cov = np.mean(summry[0])
+        uwpd_ = np.mean(summry[1]) 
+
+        self.agd_log.update_covuwpd(cov,uwpd_) 
+        self.agd_log.update_factormap(fmap)
