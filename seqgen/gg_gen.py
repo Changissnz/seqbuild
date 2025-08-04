d694c83cf78d4e3eed5add9ad11ed16b17aabbde
Richard Pham <phamrichard45@gmail.com>
2025-08-04 17:03:46 -0400

    minor code additions to file<gg_gen>

--- seqgen/gg_gen.py
+++ seqgen/gg_gen.py
@@ -6,6 +6,11 @@ def mod_uwpd_of_sequence(S,m):
     return uwpd(S_,pairwise_op=lambda x1,x2: np.abs(x2 - x1),\
         accum_op=lambda x1,x2: x1 + x2)
 
+"""
+map with 
+key : factor : float 
+value : (uwpd of modulated sequence,coverage of modulated sequence) 
+"""
 def factorseq_to_uwpdcov_map(seq,fseq):
     uwpdcov = dict()
     for m in fseq: 
@@ -43,17 +48,15 @@ class AGV2DensityLog:
         self.refvar = None
         return 
 
+    """
+    calculates a map with 
+    key: category 
+    value: frequency of category. 
 
+    There are `cat_sz` number of categories.
+    """
     def catfreq_map(self,density_measure,lx = None):
-        q = None 
-
-        if density_measure == "cov": 
-            q = [c[0] for c in self.covuwpd_log]
-        elif density_measure == "uwpd": 
-            q = [c[1] for c in self.covuwpd_log] 
-        else:
-            assert density_measure in self.factormap_log
-            q = [c[0] for c in self.factormap_log[density_measure]]
+        q = self.fetch_vec(density_measure)
 
         clog = None 
         if type(lx) != type(None): 
@@ -65,7 +68,21 @@ class AGV2DensityLog:
         scat_vec = stdcat_vec(np.array(clog),self.cat_sz,0.0) 
         fmap = vec_to_frequency_map(scat_vec) 
         return fmap 
-        
+
+    def fetch_vec(self,density_measure):
+        assert type(density_measure) ! = type(None) 
+
+        q = None 
+        if density_measure == "cov": 
+            q = [c[0] for c in self.covuwpd_log]
+        elif density_measure == "uwpd": 
+            q = [c[1] for c in self.covuwpd_log] 
+        else:
+            assert density_measure in self.factormap_log
+            q = [c[0] for c in self.factormap_log[density_measure]]
+
+        return q 
+
     def update_covuwpd(self,cov,uwpd): 
         self.covuwpd_log.append((cov,uwpd)) 
 
@@ -104,6 +121,9 @@ class AGV2DensityLog:
 
     def update_density_count(self,v): 
         return -1 
+
+    def label_sample_vector(self,seq):
+        return -1 
         
 class AGV2GuidedGen: 
 
@@ -134,15 +154,6 @@ class AGV2GuidedGen:
         assert is_number(span,{float,np.float16,np.float32,np.float64}) 
         assert span > 1 
         self.bspan = span 
-
-    def set_refvar(self,refvar):
-        if refvar[0] == "cov": 
-            return 
-        elif refvar[0] == "uwpd": 
-            return 
-        else: 
-            # common factor 
-            return 
 
     def set_density(self,density):
         assert type(density) == np.ndarray
