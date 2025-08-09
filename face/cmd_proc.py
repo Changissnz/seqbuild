918395c646b5a99356ba37dd02286ee6135b66a3
Richard Pham <phamrichard45@gmail.com>
2025-08-09 19:38:24 -0400

    more work with file<cmd_proc>,file<comm_lang>

--- face/cmd_proc.py
+++ face/cmd_proc.py
@@ -7,6 +7,18 @@ BASE_PRNG = ["lcg","lcgv2","lcgv3","mdr","idforest","optri",\
 LANG_KEYTERMS = ["make","run","with","set","for","iter","write","to",\
             "open"]  
 
+# TODO: incomplete 
+def MAIN_object_method(q):
+
+    if type(q) in {MethodType,FunctionType}:
+        def f():
+            return q() 
+        return f 
+
+    if type(q) in {LCGV2,LCGV3}:
+        return q.__next__ 
+    return -1 
+
 def MAKE_proc(splitstr_cmd): 
     assert splitstr_cmd[0] == "make"
 
@@ -20,8 +32,29 @@ def MAKE_proc(splitstr_cmd):
         return prg__LCG(*parameters) 
     return None
 
-def RUN_proc(splitstr_cmd): 
-    return -1  
+def RUN_proc(splitstr_cmd,var_map): 
+    assert splitstr_cmd[0] == "run"
+    assert splitstr_cmd[1] in var_map 
+    
+    # get the object from the variable name     
+    var_obj = var_map[splitstr_cmd[1]] 
+
+    f = MAIN_object_method(var_obj) 
+
+    if len(splitstr_cmd) < 3:
+        return f()
+
+    lx = []
+
+    assert splitstr_cmd[2] == "for" 
+
+    iterations = int(splitstr_cmd[3]) 
+    assert iterations > 0 
+    assert splitstr_cmd[4] == "iter" 
+
+    for _ in range(iterations):
+        lx.append(f())
+    return lx 
 
 def OPEN_proc(splitstr_cmd):
     assert splitstr_cmd[0] == "open" 
