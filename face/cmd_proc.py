f7735781f4776064e061d631a4c7989610f4e3fb
Richard Pham <phamrichard45@gmail.com>
2025-08-12 14:32:05 -0400

    add CONVERT command to file<cmd_proc>

--- face/cmd_proc.py
+++ face/cmd_proc.py
@@ -8,7 +8,9 @@ from .make_cmd import *
 import io 
 
 LANG_KEYTERMS = ["make","run","with","set","for","iter","write","to",\
-            "open"]  
+            "open","convert"]   
+
+CONVERT_TYPES = ["range","ndim","nvec","tvec"]
 
 # TODO: incomplete 
 def MAIN_method_for_object(q):
@@ -92,6 +94,45 @@ def OPEN_proc(splitstr_cmd):
         q.close() 
 
     return open(splitstr_cmd[2],"ab")
+
+"""
+convert GENERATOR to type_x ?with arg1,...,argN?.
+
+convert G to range
+convert G to ndim with 5,6,9,10 
+convert G to nvec with 6 
+convert G to tvec with 7
+""" 
+def CONVERT_proc(splitstr_cmd,var_map): 
+    assert splitstr_cmd[0] == "convert"
+
+    assert splitstr_cmd[1] in var_map
+
+    struct = var_map[splitstr_cmd[1]] 
+
+    if not type(struct) in {MethodType,FunctionType}: 
+        struct = MAIN_method_for_object(struct)
+    
+    assert splitstr_cmd[2] == "to"
+    assert splitstr_cmd[3] in CONVERT_TYPES
+
+    if splitstr_cmd[3] == "range":
+        return prg__single_to_range_outputter(struct) 
+
+    assert splitstr_cmd[4] == "with" 
+
+    if splitstr_cmd[3] == "ndim": 
+        dim = string_to_vector(splitstr_cmd[5], castFunc = int)
+        return prg__single_to_ndim_index_outputter(struct,dim)
+
+    l = string_to_vector(splitstr_cmd[5],castFunc = int) 
+    assert len(l) == 1
+    l = l[0] 
+
+    if splitstr_cmd[3] == "nvec": 
+        return prg__single_to_nvec(struct,l) 
+
+    return prg__single_to_trinary_vector(struct,l)
 
 """
 write object to file_object 
